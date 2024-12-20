e17da4609acebee58be615b7bab21b3c406e5a9a
Juliana <jsnorek340@gmail.com>
2024-12-20 15:36:03 -0800

    added comments to code files

--- week2-friday-pres/hello.py
+++ week2-friday-pres/hello.py
@@ -1,16 +1,23 @@
+# function to subract two numbers
 def subtract_numbers(a, b):
     try:
+        # try to subtract b from a
         return a - b
-    except TypeError:
+    # TypeError checks that the inputs are the same type. So if a is an int and b is a string it will flag an error
+    except TypeError: 
+        # error handling
         return "Error: Both inputs must be numbers"
 
-print(subtract_numbers(5, 2))
-print(subtract_numbers(6, '9'))
+print(subtract_numbers(5, 2)) # should return 3
+print(subtract_numbers(6, '9')) # should return "Error: Both inputs must be numbers"
 
+# function to divide two numbers
 def divide_numbers(a, b):
+    # check if demoniator is 0
     if b == 0:
+        # error handling
         return "Error: Cannot divide by zero"
     return a / b
 
-print(divide_numbers(10, 2))
-print(divide_numbers(11, 0))
+print(divide_numbers(10, 2)) # should return 5
+print(divide_numbers(11, 0)) # should return "Error: Cannot divide by zero"
--- week2-friday-pres/test_hello.py
+++ week2-friday-pres/test_hello.py
@@ -1,14 +1,22 @@
 import unittest
 from hello import subtract_numbers, divide_numbers
 
+# test case class is a subclass of unittest.TestCase in order to use methods and attributes in unittest import
 class Test(unittest.TestCase):
+    # test method for subtract_numbers function
     def test_subtract_numbers(self):
-        self.assertEqual(subtract_numbers(9, 3), 6)
+        # check if subtract_numbers(9, 3) equals 6
+        self.assertEqual(subtract_numbers(9, 3), 6) # self refers to the instance of the Test class
+        # check if subtract_numbers(-1, -4) equals 3
         self.assertEqual(subtract_numbers(-1, -4), 3)
 
+    # test method for divide_numbers function
     def test_divide_numbers(self):
+        # check if divide_numbers(10, 5) equals 2
         self.assertEqual(divide_numbers(10, 5), 2)
+        # check if divide_numbers(-4, 2) equals -2
         self.assertEqual(divide_numbers(-4, 2), -2)
 
+# ensures test case, unittest.main() runs when the script is run directly, not when it's imported
 if __name__ == '__main__':
     unittest.main()
